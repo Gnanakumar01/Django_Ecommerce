6a074f18bb0871ac1b36e71fe3a8c6de8832f6a0
Gnanakumar01 <sgnanakumar929@gmail.com>
2026-01-05 11:42:56 +0530

    Updated

--- Eshop/Eshop/settings.py
+++ Eshop/Eshop/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Apps
-    'mainapp'
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,9 @@ ROOT_URLCONF = 'Eshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
