3119150467dd01f0856b2e2b9be2fbab55253f82
Gnanakumar01 <sgnanakumar929@gmail.com>
2026-01-21 12:54:17 +0530

    Modified

--- Eshop/products/urls.py
+++ Eshop/products/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 
 from .views import productView, searchProducts
 
+from .views import (CreateProduct, ProductDetail, 
+                    UpdateProduct, DeleteProduct)
+
 urlpatterns = [
-    path('products/', productView, name = 'products_page'),
-    path('search/', searchProducts, name = 'search_products')
+    path('all/', productView, name = 'products_page'),
+    path('search/', searchProducts, name = 'search_products'),
+
+    path('add/', CreateProduct.as_view(), name='add_product'),
+    path('<int:pk>/', ProductDetail.as_view(), name='product_details'),
+    path('<int:pk>/edit/', UpdateProduct.as_view(), name='edit_product'),
+    path('<int:pk>/delete/', DeleteProduct.as_view(), name='delete_product')
 ]
 
-# from . import views
-# urlpatterns = [
-#     path('products/', views.productView, name= 'products_page'),
-#     path('search/', views.searchProducts, name = 'search_products')
-# ]
